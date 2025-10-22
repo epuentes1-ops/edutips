0902d66d476dede4747db5212e8711170d49f43e
epuentes1-ops <epuentesespitia@gmail.com>
2025-10-22 16:10:45 -0500

    Se agregó una nueva ruta y un elemento de menú para las categorías de administrador

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,9 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('12345678')
         ]);
+
+        Category::factory(5)->create();
     }
 }
