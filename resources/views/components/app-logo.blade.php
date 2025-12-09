7af6b1da0179326f17b0a4c8ebdb395d7cb8ab37
epuentes1-ops <epuentesespitia@gmail.com>
2025-12-09 18:36:20 -0500

    Mejorada la referencia de la imagen del logo en 'app-logo.blade.php' para utilizar la función asset, asegurando una carga correcta del recurso.

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,6 @@
 
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold"><img src="images/logos/LogoEdutips.svg" alt=""></span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">
+        <img src="{{ asset('images/logos/LogoEdutips.svg') }}" alt="EduTips">
+    </span>
 </div>
